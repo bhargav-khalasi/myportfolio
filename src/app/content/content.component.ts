420748e881658a8d939cadbb10d2bb97978ae63f
Bhargav <bkhalasi404@gmail.com>
2020-07-26 09:46:53 +0530

    change image path

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -36,21 +36,21 @@ projectsData = [
     "project_name": "Segmentation of Medicine Blister Pack",
     "project_desc": "Used Image Processing (with Opencv and Python) to segment out \
     essential information (like expiry date, MRP, etc) from medicine blister pack",
-    "image_path": "/assets/images/med3.png"
+    "image_path": "assets/images/med3.png"
   },
   {
     "project_name": "Data Hiding in Image",
     "project_desc": "Developed software in Java that hides data in an image by\
      manipulating its pixels unnoticeable to Human eye and successfully\
     retrieves the data back.",
-    "image_path": "/assets/images/img_hide.jpg"
+    "image_path": "assets/images/img_hide.jpg"
   },
   {
     "project_name": "Real time Chat Web-app",
     "project_desc": "Developed One to One Real time Chat Application using \
     NodeJS(Using SocketIO, Express, Mongoose frameworks) as backend and \
     JavaScript as frontend with MongoDB as Database.",
-    "image_path": "/assets/images/img_chat.JPG"
+    "image_path": "assets/images/img_chat.JPG"
   }
 ];
   ngOnInit(): void {
