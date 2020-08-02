882628eb7adcd9c633e322a7ee01e87b63ab3360
Bhargav <bkhalasi404@gmail.com>
2020-08-02 21:45:58 +0530

    chatbox

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,21 @@ import { SideMenuComponent } from './side-menu/side-menu.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ContentComponent } from './content/content.component';
 import { FormsModule } from '@angular/forms';
+import { ChatbotComponent } from './chatbot/chatbot.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
     AppComponent,
     SideMenuComponent,
-    ContentComponent
+    ContentComponent,
+    ChatbotComponent
   ],
   imports: [
     BrowserModule,
     NgbModule,
-    FormsModule
+    FormsModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
